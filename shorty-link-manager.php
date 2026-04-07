02a355a533e7ecff99575414d8f032ff48964a23
AddonDeveloper <63096670+AddonDeveloper@users.noreply.github.com>
2026-04-07 07:38:10 +0200

    Update author name in shorty-link-manager.php

--- shorty-link-manager.php
+++ shorty-link-manager.php
@@ -5,7 +5,7 @@
  * Version: 0.1
  * Requires at least: 6.0
  * Requires PHP: 7.4
- * Author: theveloper
+ * Author: AddonDeveloper
  * License: GPL-2.0-or-later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: shorty-link-manager
